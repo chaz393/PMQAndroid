fe328afd7d399dfec9416cfc07f47b5e3962526c
Chaz Lakinger <clakinger@gmail.com>
2020-01-19 16:10:09 -0600

    -testing

--- app/src/main/java/com/partymusicq/android/MainActivity.kt
+++ app/src/main/java/com/partymusicq/android/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //this is a test
+        //this is a test2
     }
 }
